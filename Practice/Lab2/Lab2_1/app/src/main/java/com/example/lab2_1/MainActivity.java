640cb60ca62899a29e427db6945aaabede2b5b22
NgTrGKiet <kierngr@gmail.com>
2024-04-01 23:45:45 +0700

    lab2_1, lab2_2

--- Practice/Lab2/Lab2_1/app/src/main/java/com/example/lab2_1/MainActivity.java
+++ Practice/Lab2/Lab2_1/app/src/main/java/com/example/lab2_1/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.lab2_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView lvPerson;
+    private TextView tvSelection;
+    private final String arr[] = {"Teo", "Ty", "Bin", "Bo"};
+
+    private void initUi()
+    {
+        lvPerson = (ListView)findViewById(R.id.lv_person);
+        tvSelection = (TextView)findViewById(R.id.tv_selection);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUi();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr);
+
+        lvPerson.setAdapter(adapter);
+
+        lvPerson.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?>arg0, View arg1, int arg2, long arg3) {
+                //đối số arg2 là vị trí phần tử trong Data Source (arr)
+                tvSelection.setText("position :" + arg2 + "; value =" + arr[arg2]);
+            }
+        });
     }
 }
