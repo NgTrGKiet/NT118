15e60d8a3785db7bd5771e95fc204a652242eeba
NgTrGKiet <kierngr@gmail.com>
2024-05-30 19:20:59 +0700

    lab5 + lab6

---
+++ Practice/Lab6/Lab6_4/app/src/main/java/com/example/lab6_4/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lab6_4;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new frag1(), "ViewPage 1");
+        vpAdapter.addFragment(new frag2(), "ViewPage 2");
+        vpAdapter.addFragment(new frag3(), "ViewPage 3");
+        viewPager.setAdapter(vpAdapter);
+    }
+}
